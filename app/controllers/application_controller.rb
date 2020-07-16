faa7343b6b99fa5100ebaafebfa55ea9cc3b3c9a
francepants <franceline90@gmail.com>
2020-07-16 12:06:12 -0500

    added gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, 'secret'
+    # set :session_secret, ENV['SINATRA_SECRET']
   end
 
   get '/' do
