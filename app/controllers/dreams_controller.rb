0c419e5b53e3f22f9a740f9a4a5e7dabe9929365
francepants <franceline90@gmail.com>
2020-07-23 08:46:06 -0500

    removed comments

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -2,23 +2,17 @@ class DreamsController < ApplicationController
 
     use Rack::Flash
 
-    # READ 1 - read a list of dreams
     get '/dreams' do
-        # @dreams = Dream.all
         @dreams = current_user.dreams
         erb :'dreams/index'
     end
 
-    # CREATE 1 - get a new dream form
     get '/dreams/new' do
         erb :'dreams/new'
     end
 
-    # CREATE 2 - post the new dream form
     post '/dreams' do
         dream = current_user.dreams.build(params[:dream])
-        # the above line is associated build. current_user is what you want to associate it to. creates a dream with a foreign key already filled out. this is the has_many version
-        # the belongs_to would be @user = Dream.build_user() - would build user based on dreams
         if dream.save
             redirect '/dreams'
         else
@@ -26,10 +20,8 @@ class DreamsController < ApplicationController
         end
     end
 
-    # UPDATE 1 - edits the dream
     get '/dreams/:id/edit' do
         set_dream
-        #doesnt allow you to edit someone else's dream entry
         if current_user == @dream.user
             erb :'dreams/edit'
         else
@@ -37,18 +29,20 @@ class DreamsController < ApplicationController
         end
     end
 
-    # READ 2 - view more details about a dream
     get '/dreams/:id' do
         set_dream
-        if set_dream
+        if current_user == @dream.user
             erb :'dreams/show'
         else
             redirect '/dreams'
         end
-        #params is a hash for accessing a key called id, which is given by our route variable
+        # if set_dream
+        #     erb :'dreams/show'
+        # else
+        #     redirect '/dreams'
+        # end
     end
 
-    # UPDATE 2 - patch request to update the dream
     patch '/dreams/:id' do
         set_dream
         if current_user == @dream.user && @dream.update(
@@ -63,7 +57,6 @@ class DreamsController < ApplicationController
         end
     end
 
-    # DELETE
     delete '/dreams/:id' do
         set_dream
         if current_user == @dream.user
@@ -77,6 +70,5 @@ class DreamsController < ApplicationController
         def set_dream
             @dream = Dream.find_by_id(params[:id])
         end
-        # id lives in params[:id]
 
 end
